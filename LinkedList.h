a9a5623fb67d9cb06c072990b888b69d3ae79634
thereconjacob <jvpokeyjake@gmail.com>
2023-04-28 14:19:40 +0100

    Do exercise 2

--- LinkedList.h
+++ LinkedList.h
@@ -1,15 +1,10 @@
 #pragma once
 #include "Link.h"
-class LinkedList
-{
+class LinkedList {
 public:
 	Link* first;
-
-	Link* Delete(int key);
-	void Display();
-	Link* Find(int key);
-	void Insert(Link* newLink);
-	bool IsEmpty();
 	LinkedList();
+	bool IsEmpty();
+	void Insert(Link* newLink);
+	void Display();
 };
-
